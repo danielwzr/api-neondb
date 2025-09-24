3d37237ab0982513a829714e895c111d0d21994a
Daniel Andrade <49415199+danielwzr@users.noreply.github.com>
2025-09-24 17:03:05 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  connectionString: 'postgresql://neondb_owner:npg_Cco3nqTpSg9I@ep-tiny-cloud-ad5dwyl7-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require',
+  ssl: {
+    require: true,           // força uso de SSL
+    rejectUnauthorized: false // ignora self-signed na cadeia
+  }
+});
+
+async function testConnection() {
+  try {
+    const res = await pool.query("SELECT NOW()");
+    console.log("Conexão bem sucedida:", res.rows[0]);
+  } catch (err) {
+    console.error("Erro na conexão:", err);
+  }
+}
+
+testConnection();
